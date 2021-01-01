7d1a98b708ed3385ccb28eb41545f765ccf7d9ed
Ezekiel Rubio <rubioez@oregonstate.edu>
2020-12-31 16:19:12 -0800

    updated spelling for author

--- btc-sentiment-analysis/setup.py
+++ btc-sentiment-analysis/setup.py
@@ -6,7 +6,7 @@ with open("README.md", "r") as fh:
 setup(
 	name='btc-sentiment-analysis',
 	version='0.0.2',
-	autho='Diego Figueroa',
+	author='Diego Figueroa',
 	author_email='diegofigueroa79@gmail.com',
 	description='A small sentiment analysis library for bitcoin',
 	long_description=long_description,
